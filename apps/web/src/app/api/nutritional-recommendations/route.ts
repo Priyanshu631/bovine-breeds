bcb42b8494bb10ba92080327c52324d032ba4af7
Priyanshu Ranjan <154614838+Priyanshu631@users.noreply.github.com>
2025-09-26 15:06:36 +0530

    changed to latest flash

--- apps/web/src/app/api/nutritional-recommendations/route.ts
+++ apps/web/src/app/api/nutritional-recommendations/route.ts
@@ -7,7 +7,7 @@ if (!apiKey) {
 }
 
 const genAI = new GoogleGenerativeAI(apiKey);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-002" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
 
 export async function POST(req: NextRequest) {
   try {
